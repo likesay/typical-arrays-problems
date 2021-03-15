303d7980d0d8555f9d8ae70393828a8e0425215e
Aliaksei Panko <79204279+likesay@users.noreply.github.com>
2021-03-15 19:30:49 +0300

    Update index.js
    add logic

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minValue = null;
+  if(array == null || array.length == 0){
+return 0;
+  }
+  array.forEach(element => {
+    if (minValue == null){
+      minValue = element;
+    } else if(element < minValue) {
+      minValue = element;
+    }
+  });
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxValue = null;
+  if(array == null || array.length == 0){
+    return 0;
+      }
+  array.forEach(element => {
+    if (maxValue == null){
+      maxValue = element;
+    } else if(element > maxValue) {
+      maxValue = element;
+    }
+  });
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
+ let sum = 0;
+ let count = 0;
+ if(array == null || array.length == 0){
   return 0;
+ }
+ array.forEach(elem => {
+   sum += elem;
+   count += 1;
+ });
+ if(count == 0){
+   return 0;
+ }
+  return sum/count;
 }
